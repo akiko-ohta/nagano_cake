9cea6c9ed5f35c5f2cc4b95aca9bb06ab8ec35df
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-04 09:33:08 +0000

    [edit]routesを編集

--- config/routes.rb
+++ config/routes.rb
@@ -28,51 +28,27 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
-  namespace :public do
-    get 'addresses/new'
-    get 'addresses/create'
-    get 'addresses/edit'
-    get 'addresses/update'
-    get 'addresses/show'
-    get 'addresses/index'
-    get 'addresses/destroy'
+
+ scope module: :public do
+  resources :items, only: [:index, :show]
+  resources :customers, only: [:edit, :update]
+  get "customers/my_page" => "customers#show", as: "customers/show"
+  resources :customers do
+    collection do
+      get "confirm_withdraw"
+      patch "withdraw"
+    end
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/confirm_withdraw'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  get 'homes/top'
+  get '/about' => "homes#about", as: "homes/about"
+end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
-}
+  }
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  root to: "public/homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
