5237392c779b93bb8f2687eaec50fb0c38f287a2
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-04 07:06:29 +0000

    [add]コントローラーの追加

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,22 @@
+class Admin::GenresController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def unvailable
+  end
+end
