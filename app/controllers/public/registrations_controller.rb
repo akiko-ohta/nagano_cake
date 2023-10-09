11aba785f1be81688a74d9fac3658a26107380a3
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-09 03:12:54 +0000

    [edit]ユーザ詳細画面の編集

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,6 +2,10 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path(resource)
+    customers_show_path
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
