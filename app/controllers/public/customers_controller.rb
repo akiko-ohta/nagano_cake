6f0f3bd82d6897db26702d64dec200faa6fcab3c
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-14 03:53:41 +0000

    [add]会員管理、編集画面の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,4 +14,11 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
+  end
+
 end
