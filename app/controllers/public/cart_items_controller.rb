d0d47d159d1d50415bcfcb13b2bbebc6ccdf08c3
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-18 13:57:09 +0000

    [edit]カートの編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,20 +14,20 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-    current_user.cart_items.destroy_all
+    current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
 
   def create
-    cart_item = CartItem.find_by(imte_id: params[:item_id])
-    if cart_item.item_id == @cart_item.item_id
-      cart_item.amount + @cart_item.amount
-      cart_item.update
+    cart_item = CartItem.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(amount: params[:cart_item][:amount])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
       redirect_to cart_items_path
     else
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.save
-    redirect_to cart_items_path
+      cart_item.save
+      redirect_to cart_items_path
     end
   end
 
