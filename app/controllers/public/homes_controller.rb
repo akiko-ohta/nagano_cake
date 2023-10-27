152d328a43b6c536def24f2675b17ed8ddb808e4
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-10-27 09:09:41 +0000

    [edit]レイアウトの編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
-    @items = Item.all
+    @items = Item.page(params[:page]).per(4)
+    @genre = Genre.find_by(params[:genre_name])
   end
 
   def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @genres = Genre.all
+    @items = Item.page(params[:page]).per(8)
+    @genre = Genre.find_by(params[:genre_name])
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
+    @genre = Genre.find_by(params[:genre_name])
     @cart_item = CartItem.new
   end
 end
